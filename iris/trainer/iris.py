547d4461927b7359c52f34b9b7cb2acfeb98005c
Guy Hummel <guyhummel@users.noreply.github.com>
2020-12-01 13:26:20 -0700

    Add files via upload

--- iris/trainer/iris.py
+++ iris/trainer/iris.py
@@ -16,12 +16,6 @@
 # limitations under the License.
 # ## TensorFlow programming
 # 
-# This guide uses these high-level TensorFlow concepts:
-# 
-# * Use TensorFlow's default [eager execution](../../guide/eager.ipynb) development environment,
-# * Import data with the [Datasets API](../../guide/datasets.ipynb),
-# * Build models and layers with TensorFlow's [Keras API](../../guide/keras/overview.ipynb).
-# 
 # This tutorial is structured like many TensorFlow programs:
 # 
 # 1. Import and parse the dataset.
@@ -37,224 +31,183 @@
 # Import TensorFlow and the other required Python modules.
 
 import os
+import argparse
 import tensorflow as tf
 
 
-# ## Import and parse the training dataset
-# 
-# Download the dataset file and convert it into a structure that can be used by this Python program.
-# 
-# ### Download the dataset
-# 
-# Download the training dataset file using the `tf.keras.utils.get_file` function. This returns the file path of the downloaded file:
+def get_args():
+    """Argument parser.
 
-train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
-
-train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
-                                           origin=train_dataset_url)
-
-
-# column order in CSV file
-column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
-
-feature_names = column_names[:-1]
-label_name = column_names[-1]
+    Returns:
+      Dictionary of arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--job-dir',
+        type=str,
+        required=True,
+        help='local or GCS location for writing checkpoints and exporting '
+             'models')
+    args, _ = parser.parse_known_args()
+    return args
 
 
-# Each label is associated with string name (for example, "setosa"), but machine learning typically relies on numeric values. The label numbers are mapped to a named representation, such as:
-# 
-# * `0`: Iris setosa
-# * `1`: Iris versicolor
-# * `2`: Iris virginica
-# 
+def train_and_evaluate(args):
 
-class_names = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
+    # ## Import and parse the training dataset
+    # 
+    # Download the dataset file and convert it into a structure that can be used by this Python program.
+    # 
+    # ### Download the dataset
+    # 
+    # Download the training dataset file using the `tf.keras.utils.get_file` function. This returns the file path of the downloaded file:
 
+    train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
 
-# ### Create a `tf.data.Dataset`
-# 
-# TensorFlow's [Dataset API](../../guide/data.ipynb) handles many common cases for loading data into a model. This is a high-level API for reading data and transforming it into a form used for training.
-# 
-# 
-
-batch_size = 32
-
-train_dataset = tf.data.experimental.make_csv_dataset(
-    train_dataset_fp,
-    batch_size,
-    column_names=column_names,
-    label_name=label_name,
-    num_epochs=1)
+    train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
+                                               origin=train_dataset_url)
 
 
-# The `make_csv_dataset` function returns a `tf.data.Dataset` of `(features, label)` pairs, where `features` is a dictionary: `{'feature_name': value}`
+    # column order in CSV file
+    column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+
+    feature_names = column_names[:-1]
+    label_name = column_names[-1]
 
 
-# To simplify the model building step, create a function to repackage the features dictionary into a single array with shape: `(batch_size, num_features)`.
-# 
-# This function uses the `tf.stack` method which takes values from a list of tensors and creates a combined tensor at the specified dimension:
+    # Each label is associated with string name (for example, "setosa"), but machine learning typically relies on numeric values. The label numbers are mapped to a named representation, such as:
+    # 
+    # * `0`: Iris setosa
+    # * `1`: Iris versicolor
+    # * `2`: Iris virginica
+    # 
 
-def pack_features_vector(features, labels):
-  """Pack the features into a single array."""
-  features = tf.stack(list(features.values()), axis=1)
-  return features, labels
-
-
-# Then use the `tf.data.Dataset#map` method to pack the `features` of each `(features,label)` pair into the training dataset:
-
-train_dataset = train_dataset.map(pack_features_vector)
+    class_names = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
 
 
-# ### Create a model using Keras
-# 
-# The TensorFlow `tf.keras` API is the preferred way to create models and layers. This makes it easy to build models and experiment while Keras handles the complexity of connecting everything together.
-# 
-# The `tf.keras.Sequential` model is a linear stack of layers. Its constructor takes a list of layer instances, in this case, one input layer with 4 nodes, two `tf.keras.layers.Dense` layers with 10 nodes each, and an output layer with 3 nodes representing our label predictions.
+    # ### Create a `tf.data.Dataset`
+    # 
+    # TensorFlow's [Dataset API](../../guide/data.ipynb) handles many common cases for loading data into a model. This is a high-level API for reading data and transforming it into a form used for training.
+    # 
+    # 
 
-model = tf.keras.Sequential([
-  tf.keras.layers.InputLayer(input_shape=(4,)),
-  tf.keras.layers.Dense(10, activation=tf.nn.relu),
-  tf.keras.layers.Dense(10, activation=tf.nn.relu),
-  tf.keras.layers.Dense(3)
-])
+    batch_size = 32
 
-
-# The *[activation function](https://developers.google.com/machine-learning/crash-course/glossary#activation_function)* determines the output shape of each node in the layer. These non-linearities are important—without them the model would be equivalent to a single layer. There are many `tf.keras.activations`, but [ReLU](https://developers.google.com/machine-learning/crash-course/glossary#ReLU) is common for hidden layers.
-# 
-# The ideal number of hidden layers and neurons depends on the problem and the dataset. Like many aspects of machine learning, picking the best shape of the neural network requires a mixture of knowledge and experimentation. As a rule of thumb, increasing the number of hidden layers and neurons typically creates a more powerful model, which requires more data to train effectively.
+    train_dataset = tf.data.experimental.make_csv_dataset(
+        train_dataset_fp,
+        batch_size,
+        column_names=column_names,
+        label_name=label_name,
+        num_epochs=1)
 
 
-# ## Train the model
-# 
-# ### Define the loss and gradient function
-# 
-# Both training and evaluation stages need to calculate the model's *[loss](https://developers.google.com/machine-learning/crash-course/glossary#loss)*. This measures how off a model's predictions are from the desired label, in other words, how bad the model is performing. We want to minimize, or optimize, this value.
-# 
-# Our model will calculate its loss using the `tf.keras.losses.SparseCategoricalCrossentropy` function which takes the model's class probability predictions and the desired label, and returns the average loss across the examples.
-
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-def loss(model, x, y):
-  y_ = model(x)
-
-  return loss_object(y_true=y, y_pred=y_)
+    # The `make_csv_dataset` function returns a `tf.data.Dataset` of `(features, label)` pairs, where `features` is a dictionary: `{'feature_name': value}`
 
 
-# Use the `tf.GradientTape` context to calculate the *[gradients](https://developers.google.com/machine-learning/crash-course/glossary#gradient)* used to optimize your model:
+    # To simplify the model building step, create a function to repackage the features dictionary into a single array with shape: `(batch_size, num_features)`.
+    # 
+    # This function uses the `tf.stack` method which takes values from a list of tensors and creates a combined tensor at the specified dimension:
 
-def grad(model, inputs, targets):
-  with tf.GradientTape() as tape:
-    loss_value = loss(model, inputs, targets)
-  return loss_value, tape.gradient(loss_value, model.trainable_variables)
-
-
-# ### Create an optimizer
-# 
-
-optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+    def pack_features_vector(features, labels):
+      """Pack the features into a single array."""
+      features = tf.stack(list(features.values()), axis=1)
+      return features, labels
 
 
-# ### Training loop
-# 
-# With all the pieces in place, the model is ready for training! A training loop feeds the dataset examples into the model to help it make better predictions. The following code block sets up these training steps:
-# 
-# 1. Iterate each *epoch*. An epoch is one pass through the dataset.
-# 2. Within an epoch, iterate over each example in the training `Dataset` grabbing its *features* (`x`) and *label* (`y`).
-# 3. Using the example's features, make a prediction and compare it with the label. Measure the inaccuracy of the prediction and use that to calculate the model's loss and gradients.
-# 4. Use an `optimizer` to update the model's variables.
-# 5. Keep track of some stats for visualization.
-# 6. Repeat for each epoch.
-# 
-# The `num_epochs` variable is the number of times to loop over the dataset collection. Counter-intuitively, training a model longer does not guarantee a better model. `num_epochs` is a *[hyperparameter](https://developers.google.com/machine-learning/glossary/#hyperparameter)* that you can tune. Choosing the right number usually requires both experience and experimentation:
+    # Then use the `tf.data.Dataset#map` method to pack the `features` of each `(features,label)` pair into the training dataset:
 
-num_epochs = 201
-
-for epoch in range(num_epochs):
-  epoch_loss_avg = tf.keras.metrics.Mean()
-  epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
-
-  # Training loop - using batches of 32
-  for x, y in train_dataset:
-    # Optimize the model
-    loss_value, grads = grad(model, x, y)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-    # Track progress
-    epoch_loss_avg.update_state(loss_value)  # Add current batch loss
-    # Compare predicted label to actual label
-    epoch_accuracy.update_state(y, model(x))
-
-  # End epoch
-
-  if epoch % 50 == 0:
-    print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                epoch_loss_avg.result(),
-                                                                epoch_accuracy.result()))
+    train_dataset = train_dataset.map(pack_features_vector)
 
 
-# ## Evaluate the model's effectiveness
-# 
-# Now that the model is trained, we can get some statistics on its performance.
-# 
-# *Evaluating* means determining how effectively the model makes predictions. To determine the model's effectiveness at Iris classification, pass some sepal and petal measurements to the model and ask the model to predict what Iris species they represent. Then compare the model's predictions against the actual label.  For example, a model that picked the correct species on half the input examples has an *[accuracy](https://developers.google.com/machine-learning/glossary/#accuracy)* of `0.5`. 
+    # ## Import and parse the test dataset
 
-# ### Setup the test dataset
-# 
-# Evaluating the model is similar to training the model. The biggest difference is the examples come from a separate *[test set](https://developers.google.com/machine-learning/crash-course/glossary#test_set)* rather than the training set. To fairly assess a model's effectiveness, the examples used to evaluate a model must be different from the examples used to train the model.
-# 
-# The setup for the test `Dataset` is similar to the setup for training `Dataset`. Download the CSV text file and parse that values, then give it a little shuffle:
+    test_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv"
 
-test_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv"
+    test_fp = tf.keras.utils.get_file(fname=os.path.basename(test_url),
+                                      origin=test_url)
 
-test_fp = tf.keras.utils.get_file(fname=os.path.basename(test_url),
-                                  origin=test_url)
+    test_dataset = tf.data.experimental.make_csv_dataset(
+        test_fp,
+        batch_size,
+        column_names=column_names,
+        label_name='species',
+        num_epochs=1,
+        shuffle=False)
 
-test_dataset = tf.data.experimental.make_csv_dataset(
-    test_fp,
-    batch_size,
-    column_names=column_names,
-    label_name='species',
-    num_epochs=1,
-    shuffle=False)
-
-test_dataset = test_dataset.map(pack_features_vector)
+    test_dataset = test_dataset.map(pack_features_vector)
 
 
-# ### Evaluate the model on the test dataset
-# 
-# Unlike the training stage, the model only evaluates a single [epoch](https://developers.google.com/machine-learning/glossary/#epoch) of the test data. In the following code cell, we iterate over each example in the test set and compare the model's prediction against the actual label. This is used to measure the model's accuracy across the entire test set:
+    # ### Create a model using Keras
+    # 
+    # The TensorFlow `tf.keras` API is the preferred way to create models and layers. This makes it easy to build models and experiment while Keras handles the complexity of connecting everything together.
+    # 
+    # The `tf.keras.Sequential` model is a linear stack of layers. Its constructor takes a list of layer instances, in this case, one input layer with 4 nodes, two `tf.keras.layers.Dense` layers with 10 nodes each, and an output layer with 3 nodes representing our label predictions.
 
-test_accuracy = tf.keras.metrics.Accuracy()
-
-for (x, y) in test_dataset:
-  logits = model(x)
-  prediction = tf.argmax(logits, axis=1, output_type=tf.int32)
-  test_accuracy(prediction, y)
-
-print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
+    model = tf.keras.Sequential([
+      tf.keras.layers.InputLayer(input_shape=(4,)),
+      tf.keras.layers.Dense(10, activation=tf.nn.relu),
+      tf.keras.layers.Dense(10, activation=tf.nn.relu),
+      tf.keras.layers.Dense(3)
+    ])
 
 
-# ## Use the trained model to make predictions
-# 
-# We've trained a model and "proven" that it's good—but not perfect—at classifying Iris species. Now let's use the trained model to make some predictions on [unlabeled examples](https://developers.google.com/machine-learning/glossary/#unlabeled_example); that is, on examples that contain features but not a label.
-# 
-# In real-life, the unlabeled examples could come from lots of different sources including apps, CSV files, and data feeds. For now, we're going to manually provide three unlabeled examples to predict their labels. Recall, the label numbers are mapped to a named representation as:
-# 
-# * `0`: Iris setosa
-# * `1`: Iris versicolor
-# * `2`: Iris virginica
+    # The *[activation function](https://developers.google.com/machine-learning/crash-course/glossary#activation_function)* determines the output shape of each node in the layer. These non-linearities are important—without them the model would be equivalent to a single layer. There are many `tf.keras.activations`, but [ReLU](https://developers.google.com/machine-learning/crash-course/glossary#ReLU) is common for hidden layers.
+    # 
+    # The ideal number of hidden layers and neurons depends on the problem and the dataset. Like many aspects of machine learning, picking the best shape of the neural network requires a mixture of knowledge and experimentation. As a rule of thumb, increasing the number of hidden layers and neurons typically creates a more powerful model, which requires more data to train effectively.
 
-predict_dataset = tf.convert_to_tensor([
-    [5.1, 3.3, 1.7, 0.5,],
-    [5.9, 3.0, 4.2, 1.5,],
-    [6.9, 3.1, 5.4, 2.1]
-])
 
-predictions = model(predict_dataset)
+    # ## Train the model
+    # 
+    # Both training and evaluation stages need to calculate the model's *[loss](https://developers.google.com/machine-learning/crash-course/glossary#loss)*. This measures how off a model's predictions are from the desired label, in other words, how bad the model is performing. We want to minimize, or optimize, this value.
+    # 
+    # Our model will calculate its loss using the `tf.keras.losses.SparseCategoricalCrossentropy` function which takes the model's class probability predictions and the desired label, and returns the average loss across the examples.
 
-for i, logits in enumerate(predictions):
-  class_idx = tf.argmax(logits).numpy()
-  p = tf.nn.softmax(logits)[class_idx]
-  name = class_names[class_idx]
-  print("Example {} prediction: {} ({:4.1f}%)".format(i, name, 100*p))
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
 
+    model.fit(train_dataset,
+              epochs=200)
+
+    loss, accuracy = model.evaluate(test_dataset)
+    print("Accuracy", accuracy)
+
+
+    # ## Use the trained model to make predictions
+    # 
+    # We've trained a model and "proven" that it's good—but not perfect—at classifying Iris species. Now let's use the trained model to make some predictions on [unlabeled examples](https://developers.google.com/machine-learning/glossary/#unlabeled_example); that is, on examples that contain features but not a label.
+    # 
+    # In real-life, the unlabeled examples could come from lots of different sources including apps, CSV files, and data feeds. For now, we're going to manually provide three unlabeled examples to predict their labels. Recall, the label numbers are mapped to a named representation as:
+    # 
+    # * `0`: Iris setosa
+    # * `1`: Iris versicolor
+    # * `2`: Iris virginica
+
+    predict_dataset = tf.convert_to_tensor([
+        [5.1, 3.3, 1.7, 0.5,],
+        [5.9, 3.0, 4.2, 1.5,],
+        [6.9, 3.1, 5.4, 2.1]
+    ])
+
+    predictions = model(predict_dataset)
+
+    for i, logits in enumerate(predictions):
+      class_idx = tf.argmax(logits).numpy()
+      p = tf.nn.softmax(logits)[class_idx]
+      name = class_names[class_idx]
+      print("Example {} prediction: {} {} ({:4.1f}%)".format(i, predictions[i], name, 100*p))
+
+
+    # ## Save the model
+
+    model.save(args.job_dir)
+
+
+if __name__ == '__main__':
+    args = get_args()
+
+    # Set verbosity
+    tf.compat.v1.logging.set_verbosity('ERROR')
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    train_and_evaluate(args)
